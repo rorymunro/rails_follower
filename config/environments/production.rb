e2634e4971aa0017149188d2c096a1a8ed253ab3
rorymunro <rorymunro123@gmail.com>
2014-07-25 12:26:25 +0100

    mailer try 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,20 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.default_url_options = {:host => 'rails-follow.herokuapp.com'}
+
+
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+
 config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port    => 25,
-  :domain  => 'rails-follow.herokuapp.com'
+address: "smtp.gmail.com",
+port: 587,
+domain: ENV["gmail.com"],
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["rorymunro123@gmail.com"],
+password: ENV["1marmarmaro"]
 }
 end
