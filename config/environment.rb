88e77939205180308a7d7337ccae9f0e5f4fb8f4
rorymunro <rorymunro123@gmail.com>
2014-07-25 12:08:48 +0100

    add devise mailer that works locally

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,16 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
    :tls => true,
-   :address => "smtp.gmail.com",
+   :address => "smtp.rorymunro123@gmail.com",
    :port => 587,
    :domain => "gmail.com",
    :authentication => :login,
    :user_name => "rorymunro123@gmail.com",
    :password => "1marmarmaro"
  }
+
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 config.action_mailer.default_url_options = {:host => 'rails-follow.herokuapp.com'}
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
+  :address => "stmp.rails-follow.herokuapp.com",
   :port    => 25,
   :domain  => 'rails-follow.herokuapp.com'
 }
